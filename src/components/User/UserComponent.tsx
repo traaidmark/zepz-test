f9233897be81aca33baceb8a4308fd984b7a7c2c
Adrian Kirsten <adrian@wealthpoint.app>
2023-05-29 14:42:39 +0200

    feature: adding the drawer functionality

--- src/components/User/UserComponent.tsx
+++ src/components/User/UserComponent.tsx
@@ -1,4 +1,5 @@
 import { NoSymbolIcon, HeartIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 import './style.css';
 
@@ -8,14 +9,29 @@ import { useActions } from '../../hooks/useActions';
 
 const UserComponent: React.FC<UserModel> = (props) => {
 
+  const [drawerActive, setDrawer] = useState(false);
+
   const {blockUser, toggleFollow} = useActions();
+
+  const handleDrawer = () => {
+    if(props.isBlocked) {
+      return setDrawer(false);
+    }
+    return drawerActive ? setDrawer(false) : setDrawer(true);
+  }
 
   const blockedClass = props.isBlocked ? 'feature-user--blocked' : '';
   const followedClass = props.isFollowed ? 'feature-user--followed' : '';
+  const drawerClass = drawerActive ? 'feature-user--active-drawer' : '';
 
   return (
-    <article className={`feature-user ${blockedClass} ${followedClass}`}>
-      <header>
+    <article className={`feature-user ${drawerClass} ${blockedClass} ${followedClass}`} onClick={() => handleDrawer()}>
+      
+      <header className='feature-user__header'>
+        <h4>{props.name}</h4>
+        <small>{props.reputation}</small>
+      </header>
+      <div className='feature-user__info'>
         <img src={props.imgUrl} alt={props.name} />
         {
           (props.isBlocked || props.isFollowed) && (
@@ -25,21 +41,20 @@ const UserComponent: React.FC<UserModel> = (props) => {
             </figure>
           )
         }
-      </header>
-      <div>
-        <h4>{props.name}</h4>
-        <small>{props.reputation}</small>
-      </div>
-      <footer>
-        {!props.isBlocked ? (
-          <button onClick={() => toggleFollow(props.account_id)}>
-            {props.isFollowed ? 'Unfollow' : 'Follow'}
+        <nav className='feature-user__nav'>
+          {!props.isBlocked ? (
+            <button onClick={() => toggleFollow(props.account_id)}>
+              <HeartIcon />
+              {props.isFollowed ? 'Unfollow' : 'Follow'}
+            </button>
+          ) : <span></span> }
+          <button onClick={() => blockUser(props.account_id)}>
+          <NoSymbolIcon />
+          {props.isBlocked ? 'Unblock' : 'Block'}
           </button>
-        ) : <span></span> }
-        <button onClick={() => blockUser(props.account_id)}>
-        {props.isBlocked ? 'Unblock' : 'Block'}
-        </button>
-      </footer>
+        </nav>
+      </div>
+      
     </article>
   )
 }
