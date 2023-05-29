34681bdb2a625a3874fffc1d9cdc1e118f30d8b3
Adrian Kirsten <adrian@wealthpoint.app>
2023-05-29 14:47:40 +0200

    refactor: nicer header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import './style.css';
 const HeaderComponent: React.FC = () => {
   return (
-    <aside className='feature-sidebar'>
-      <header>
+    <header className='feature-header'>
+      <div>
         <img src="/favicon.png" alt="dot" />
-        <h2>Hello, <strong>Zepz</strong>!</h2>
-        <p>These are the top 20 users from Stack overflow!</p>
-      </header>
-    </aside>
+        <h1>Hello, <strong>Zepz</strong>!</h1>
+        <p>These are the top 20 users from Stack overflow! It works through React & Redux and was a fun thing to build</p>
+      </div>
+    </header>
   )
 };
 
